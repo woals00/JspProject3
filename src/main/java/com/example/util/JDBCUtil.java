91035439e3ac79248d34829b81752f0b708f3d8e
jaeminjeong <21900675@handong.edu>
2022-11-16 19:47:44 +0900

    Initial commit

---
+++ src/main/java/com/example/util/JDBCUtil.java
@@ -0,0 +1,25 @@
+package com.example.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCUtil {
+	public static Connection getConnection(){  
+	    Connection con=null;  
+	    try{  
+	        Class.forName("com.mysql.jdbc.Driver");  
+	        con= DriverManager.getConnection("jdbc:mysql://walab.handong.edu:3306/p3_21900675","p3_21900675","Fah3aichuw");
+	    }catch(Exception e){
+	    	System.out.println(e);
+	    }  
+	    return con;  
+	}  
+	
+//	public static void main(String ars[]) {
+//		Connection conn = getConnection();
+//		if(conn != null)
+//			System.out.println("DB 연결됨!");
+//		else
+//			System.out.println("DB 연결중 오류 !");
+//	}
+}
